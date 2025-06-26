684bfeae9164aba78be556eb8219ddadf0ccff28
LucasSchurkens <schurkenslucas@gmail.com>
2025-06-26 16:48:04 -0300

    Front-end: agragando modificaciones del cliente/producto

--- frontend/cliente/productos/productos.js
+++ frontend/cliente/productos/productos.js
@@ -1,8 +1,18 @@
 let ListaPrueba = [
     {id:1,nombre:"Abrochadora",precio:2500,categoria:"Utiles",url_imagen:"../../../assets/img/productos/abrochadora.png"},
-    {id:2,nombre:"asesinatoOrientExpress",precio:3000,categoria:"Libro",url_imagen:"../../../assets/img/productos/asesinatoOrientExpress.png"},
+    {id:2,nombre:"Asesinato en el Orient Express",precio:3000,categoria:"Libro",url_imagen:"../../../assets/img/productos/asesinatoOrientExpress.png"},
     {id:3,nombre:"Cartuchera",precio:500,categoria:"Utiles",url_imagen:"../../../assets/img/productos/cartucheraStandard.png"},
-    {id:4,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
+    {id:4,nombre:"A Cuatro Vientos",precio:25000,categoria:"Libro",url_imagen:"../../../assets/img/productos/aCuatroVientos1.png"},
+    {id:5,nombre:"Bibliografo",precio:2500,categoria:"Utiles",url_imagen:"../../../assets/img/productos/bibliorato.png"},
+    {id:6,nombre:"Voligoma",precio:250,categoria:"Utiles",url_imagen:"https://i.postimg.cc/3JqRXPQH/boligoma.png"},
+    {id:7,nombre:"Libro Historia",precio:250,categoria:"Utiles",url_imagen:"https://i.postimg.cc/cH5yqGmx/historia-Santillana.jpg"},
+    {id:8,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
+    {id:9,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
+    {id:10,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
+    {id:11,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
+    {id:12,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
+    {id:13,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
+    {id:14,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"}
 ]
 
 
