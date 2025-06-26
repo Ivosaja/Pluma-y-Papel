d2a02931d9afe37b647d040e4fde443d3e5f8bdb
LucasSchurkens <schurkenslucas@gmail.com>
2025-06-26 18:08:30 -0300

    Front-end: agregando el footer y ciertas configuraciones de css del Cliente/productos

--- frontend/cliente/productos/productos.js
+++ frontend/cliente/productos/productos.js
@@ -1,18 +1,12 @@
 let ListaPrueba = [
     {id:1,nombre:"Abrochadora",precio:2500,categoria:"Utiles",url_imagen:"../../../assets/img/productos/abrochadora.png"},
-    {id:2,nombre:"Asesinato en el Orient Express",precio:3000,categoria:"Libro",url_imagen:"../../../assets/img/productos/asesinatoOrientExpress.png"},
+    {id:2,nombre:"Asesinato en el orient express",precio:3000,categoria:"Libros",url_imagen:"../../../assets/img/productos/asesinatoOrientExpress.png"},
     {id:3,nombre:"Cartuchera",precio:500,categoria:"Utiles",url_imagen:"../../../assets/img/productos/cartucheraStandard.png"},
-    {id:4,nombre:"A Cuatro Vientos",precio:25000,categoria:"Libro",url_imagen:"../../../assets/img/productos/aCuatroVientos1.png"},
+    {id:4,nombre:"A Cuatro Vientos",precio:25000,categoria:"Libros",url_imagen:"../../../assets/img/productos/aCuatroVientos1.png"},
     {id:5,nombre:"Bibliografo",precio:2500,categoria:"Utiles",url_imagen:"../../../assets/img/productos/bibliorato.png"},
     {id:6,nombre:"Voligoma",precio:250,categoria:"Utiles",url_imagen:"https://i.postimg.cc/3JqRXPQH/boligoma.png"},
     {id:7,nombre:"Libro Historia",precio:250,categoria:"Utiles",url_imagen:"https://i.postimg.cc/cH5yqGmx/historia-Santillana.jpg"},
-    {id:8,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
-    {id:9,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
-    {id:10,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
-    {id:11,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
-    {id:12,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
-    {id:13,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"},
-    {id:14,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"}
+    {id:8,nombre:"Clips",precio:250,categoria:"Utiles",url_imagen:"../../../assets/img/productos/clips.png"}
 ]
 
 
@@ -29,7 +23,7 @@ function mostrarProductos(lista){
                             <div class="tarjeta-producto">
                                 <img class="imagen-Producto" src=${lista[i].url_imagen}>
                                 <h3>${lista[i].nombre}</h3>
-                                <p>Precio:$${lista[i].precio}</p>
+                                <p>Precio:$ ${lista[i].precio}</p>
                                 <button class="boton-carrito" onClick="agregarCarrito( ${lista[i].id})">Agregar a carrito</button>
                             </div>`                                                 
     };
@@ -57,7 +51,7 @@ let filtradoLibros= document.getElementById("filtrarLibros")
 filtradoLibros.addEventListener("click",function(){
 
     let filtrados = ListaPrueba.filter(producto=>
-        producto.categoria==="Libro"
+        producto.categoria==="Libros"
     )
     mostrarProductos(filtrados)
 
