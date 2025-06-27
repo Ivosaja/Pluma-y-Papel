8c0576b988fd2c488c34e3b31fb7d67716958d34
Ivosaja <ivoliobel@gmail.com>
2025-06-26 23:23:58 -0300

    Frontend: Modularizacion de funcion guardarNombreUsuario en pantalla principal de bienvenida

--- frontend/cliente/bienvenida/bienvenida.js
+++ frontend/cliente/bienvenida/bienvenida.js
@@ -1,18 +1,18 @@
-let form = document.querySelector(".form-ingresar");
+const form = document.querySelector(".form-ingresar");
+const input =  document.getElementById("ingreso-nombre");
 
+function guardarNombreUsuario(){
+    form.addEventListener("submit",(event) =>{
+        event.preventDefault(); //prevenimos que se envie por defecto el form
+        window.location.href="../productos/productos.html" // cambiamos la localizacion de la ventana a otra cuando se haga click en el boton de submit
+        
+        console.log(input.value);
+        // Guardamos en session storage el nombre del usuario ingresado por el input del formulario
+        sessionStorage.setItem("nombreUsuario", input.value);
+    
+    })
+}
 
-let input =  document.getElementById("ingreso-nombre");
-
-form.addEventListener("submit",(event)=>{
-
-    event.preventDefault(); //prevenimos que se envie por defecto el form
-    window.location.href="../productos/productos.html"
-
-
-    console.log(input.value);
-
-    sessionStorage.setItem("nombreUsuario", input.value);
-
-})
+guardarNombreUsuario()
 
 
