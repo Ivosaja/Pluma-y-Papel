b0467ea514356b2d19275023d0c204c958817faf
Ivosaja <ivoliobel@gmail.com>
2025-06-27 17:09:32 -0300

    Frontend: Validacion de ingreso de nombre de usuario en pantalla de bienvenida agregada

--- frontend/cliente/bienvenida/bienvenida.js
+++ frontend/cliente/bienvenida/bienvenida.js
@@ -5,22 +5,22 @@ function guardarNombreUsuario(){
     form.addEventListener("submit",(event) =>{
         event.preventDefault(); //prevenimos que se envie por defecto el form
         // TODO -> Validacion de nombre de usuario
-        validarNombreUsuario()
-        
-        window.location.href="../productos/productos.html" // cambiamos la localizacion de la ventana a otra cuando se haga click en el boton de submit
-        
-        console.log(input.value);
-        // Guardamos en session storage el nombre del usuario ingresado por el input del formulario
-        sessionStorage.setItem("nombreUsuario", input.value);
-    
+        if(validarNombreUsuario()){
+            window.location.href="../productos/productos.html" // cambiamos la localizacion de la ventana a otra cuando se haga click en el boton de submit
+            console.log(input.value);
+            // Guardamos en session storage el nombre del usuario ingresado por el input del formulario
+            sessionStorage.setItem("nombreUsuario", input.value);
+        }
     })
 }
 
 function validarNombreUsuario(){
-    const inputValue = input.value
+    const inputValue = input.value.trim();
     if(inputValue.length > 10){
-        input.style.border = "red"
+        input.style.border = "2px solid #e57373"
+        return false;
     }
+    return true
 }
 
 guardarNombreUsuario()
