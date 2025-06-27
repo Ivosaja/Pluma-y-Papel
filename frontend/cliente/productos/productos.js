e37712090fbdd2f85a8952a73d0047677c470bed
Ivosaja <ivoliobel@gmail.com>
2025-06-27 19:03:33 -0300

    Frontend: Funcionalidad de agregar producto a carrito agregada!

--- frontend/cliente/productos/productos.js
+++ frontend/cliente/productos/productos.js
@@ -16,7 +16,6 @@ async function obtenerProductos() {
 
         let respuesta = await fetch("http://localhost:1001/getAllProducts");
         let data = await respuesta.json();
-
         return data.payload;
 
     }catch(error){
@@ -28,43 +27,38 @@ async function obtenerProductos() {
 ///////////////////////////////////////////////////////////////////
 
 function mostrarProductos(lista){ 
-
-
     let cartaProducto = "";
     for(let i = 0; i<lista.length; i++){
-        
             cartaProducto += `
-                            <div class="tarjeta-producto">
-                                <img class="imagen-Producto" src=${lista[i].url_imagen}>
-                                <h3>${lista[i].nombre}</h3>
-                                <p>Precio: $${lista[i].precio}</p>
-                                <div id="agregar-eliminar-producto">
-                                    <button id="boton-agregar" onClick="agregarCarrito( ${lista[i].id})">Agregar a carrito</button>
-                                    <button id="boton-eliminar"><i class="bi bi-trash3-fill"></i></button>
-                                </div>
-                            </div>`                                                 
+                <div class="tarjeta-producto">
+                    <img class="imagen-Producto" src=${lista[i].url_imagen}>
+                    <h3>${lista[i].nombre}</h3>
+                    <p>Precio: $${lista[i].precio}</p>
+                    <div id="agregar-eliminar-producto">
+                        <button id="boton-agregar" onclick="agregarCarrito(${lista[i].id_producto})">Agregar a carrito</button>
+                        <button id="boton-eliminar" onclick="borrarCarrito(${lista[i].id_producto})"><i class="bi bi-trash3-fill"></i></button>
+                    </div>
+                </div>
+                `                                              
     };
     contenedorProductos.innerHTML = cartaProducto; 
     mostrarCantidadProductosCarrito()
-
 }
 
 ///////////////////////////////////////////////////////////////////
 
 function mostrarCantidadProductosCarrito(){
-
     contenedorBtnCarrito.innerHTML = `
         <a href="../carrito/carrito.html" class="btn-carrito">
             <i class="bi bi-cart2"></i>
         </a>
         <span id="contador-carrito">${carrito.length}</span>
     `;
-    
 }
 
 ///////////////////////////////////////////////////////////////////
 
-function obtenerSesionStorage(){
+function obtenerNombreUsuarioSesionStorage(){
     let nombre = sessionStorage.getItem("nombreUsuario");
     let nuevo = `
     <i class="bi bi-person-circle"></i>
@@ -101,17 +95,26 @@ function filtrarLibros(lista){
 
 ///////////////////////////////////////////////////////////////////
 
+function agregarCarrito(idProducto){
+    const producto = listaProductos.find(producto => producto.id_producto === idProducto);
+    carrito.push(producto)
+    sessionStorage.setItem("carrito", JSON.stringify(carrito)) // guardo en session storage el carrito
+    console.log(carrito)
+}
+
+///////////////////////////////////////////////////////////////////
+
 
 async function init() {
-
+    // Espero a que se obtengan los productos y los retorne la funcion
     listaProductos = await obtenerProductos();
-
+    // Muestro los productos
     mostrarProductos(listaProductos);
 
     filtrarUtiles(listaProductos);
     filtrarLibros(listaProductos);
 
-    obtenerSesionStorage();
+    obtenerNombreUsuarioSesionStorage();
 
 }
 
