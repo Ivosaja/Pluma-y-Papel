6c76daea881fbbcb09e9cecaa4d2501edb9ab36e
Ivosaja <ivoliobel@gmail.com>
2025-07-02 01:07:30 -0300

    Backend: Agregando por parametro los controllers en productRoutes y saleRoutes

--- backend/src/api/routes/productRoutes.js
+++ backend/src/api/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import { validateId } from "../middlewares/middlewares";
+import { activateProduct, deleteProduct, getAllActiveProducts, getAllProducts, getProductById, modifyProduct, postProduct } from "../controllers/productControllers";
 
 
 
@@ -8,30 +9,30 @@ const router = Router();
 
 // Endpoint para obtener todos los productos de la base de datos
 
-router.get("/getAllProducts");
+router.get("/getAllProducts", getAllProducts);
 
 //Endpoint para obtener todos los productos activos para el cliente
-router.get("/getAllActiveProducts");
+router.get("/getAllActiveProducts", getAllActiveProducts);
 
 // Endpoint para obtener un producto por ID de la base de datos
 
-router.get("/getProductById/:id", validateId);
+router.get("/getProductById/:id", validateId, getProductById);
 
 //Endpoint para agregar un nuevo producto a la base de datos
 
-router.post("/postProduct");
+router.post("/postProduct", postProduct);
 
 // Endpoint para realizar una baja logica de un producto de la base de datos
 
-router.delete("/deleteProduct/:id", validateId);
+router.delete("/deleteProduct/:id", validateId, deleteProduct);
 
 // Endpoint para actualizar/modificar producto de la base de datos (nombre, categoria, precio, imagen)
 
-router.put("/modifyProduct/:id", validateId);
+router.put("/modifyProduct/:id", validateId, modifyProduct);
 
 //Endpoint para activar producto de la base de datos
 
-router.put("/activateProduct/:id", validateId)
+router.put("/activateProduct/:id", validateId, activateProduct)
 
 
 
--- backend/src/api/routes/saleRoutes.js
+++ backend/src/api/routes/saleRoutes.js
@@ -1,9 +1,10 @@
 import {Router} from "express"
+import { finalizePurchase } from "../controllers/saleControllers";
 
 const router = Router()
 
 // Endpoint para que el usuario realice una compra y se registre en la base de datos
 
-router.post("/finalizePurchase")
+router.post("/finalizePurchase", finalizePurchase)
 
 export default router;
