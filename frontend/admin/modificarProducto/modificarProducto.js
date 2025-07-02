5f9768c314fac80174a61f663a3985a2662975c3
Ivosaja <ivoliobel@gmail.com>
2025-07-02 18:18:11 -0300

    Frontend: Agregando funciones para obtener producto clickeado para modificar y para rellenar el formulario con la info de ese producto

---
+++ frontend/admin/modificarProducto/modificarProducto.js
@@ -0,0 +1,50 @@
+const idInput = document.getElementById('idProducto')
+const nombreProducto = document.getElementById('nombreProducto')
+const imagenProducto = document.getElementById('imagenProducto')
+const precioProducto = document.getElementById('precioProducto')
+const categoriaProducto = document.getElementById('categoriaProducto')
+
+
+async function obtenerProductoClickeadoPorId(id){
+    try{
+        let result = await fetch(`http://localhost:1001/api/products/getProductById/${id}`)
+        let data = await result.json()
+        return data
+
+    } catch(err){
+        console.error(err)
+    }
+}
+
+async function rellenarFormulario(id){
+    try{
+        const result = await obtenerProductoClickeadoPorId(id)
+        const [producto] = result.payload
+        
+        // Mejorar validacion si payload da undefined o 0
+
+        idInput.value = producto.id_producto
+        nombreProducto.value = producto.nombre
+        imagenProducto.value = producto.url_imagen
+        precioProducto.value = producto.precio
+        categoriaProducto.value = producto.categoria
+
+    } catch(err){
+        console.error("Error al rellenar el formulario", err)
+        alert("No se pudo cargar la informacion del producto")
+    }
+}
+
+
+
+async function init(){
+    const idProducto = sessionStorage.getItem("idProducto")
+    if(idProducto){
+        await rellenarFormulario(idProducto)
+    } else{
+        alert("No se selecciono ningun producto para modificar")
+    }
+
+}
+
+init()
