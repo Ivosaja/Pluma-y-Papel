6ec2f47f834ecda03dfb83bc1052b86478afade8
Ivosaja <ivoliobel@gmail.com>
2025-07-03 19:10:14 -0300

    Frontend: Funcion que sube y modifica el producto en la base de datos agregada + cambios en input de modificarProducto.html

--- frontend/admin/modificarProducto/modificarProducto.js
+++ frontend/admin/modificarProducto/modificarProducto.js
@@ -35,27 +35,44 @@ async function rellenarFormulario(id){
     }
 }
 
-async function modificarProducto(id){
+async function modificarProducto(id, data){
     try{
         let result = await fetch(`http://localhost:1001/api/products/modifyProduct/${id}`, {
             method: 'PUT',
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({
-                nombre: nombreProducto.value,
-                url_imagen: imagenProducto.value,
-                precio: precioProducto.value,
-                categoria: categoriaProducto.value
-            })
+            body: JSON.stringify(xd)
         })
-        let data = await result.json()
-        return data.message 
+        let info = await result.json()
+        return info.message 
 
     } catch (err){
         console.error(err)
-        return []
+        return "Error al hacer la peticion"
     }
+}
+
+function subirCambios(idProducto){
+    const formModificarProducto = document.getElementById('form-modificarProducto')
+    formModificarProducto.addEventListener('submit', async (event) => {
+        event.preventDefault() // prevengo el envio automatico del formulario
+
+        const formData = new FormData(event.target) // Transformo en una clase que me permite obtener todos los datos de un formulario pasandole por parametro el elemento que disparo el evento (el Formulario)
+
+        const data = Object.fromEntries(formData.entries())
+        console.log(data)
+
+        if(!data.nombre || !data.url_imagen || !data.precio || !data.categoria){
+            alert("Error, todos los campos deben ser validos")
+            return;
+        }
+
+        let respuestaProductoModificado = await modificarProducto(idProducto, data)
+        alert(respuestaProductoModificado)
+
+        window.location.href = "../dashboard/dashboard.html"
+    })
 }
 
 
@@ -67,8 +84,7 @@ async function init(){
         alert("No se selecciono ningun producto para modificar")
     }
 
-    await modificarProducto()
-
+    subirCambios(idProducto)
 }
 
 init()
