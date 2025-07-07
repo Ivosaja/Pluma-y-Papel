fbf6410cdf3e5d198e1fcab8b791cc70446b5bfc
Ivosaja <ivoliobel@gmail.com>
2025-07-07 00:12:05 -0300

    Backend: Configuracion de motor ejs, archivos estaticos y vistas en server.js

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,19 @@ import cors from "cors";
 import environments from "./src/api/config/environments.js";
 import { productRoutes, salesRoutes } from "./src/api/routes/indexRoutes.js";
 import { loggerUrl } from "./src/api/middlewares/middlewares.js";
+import { __dirname, join } from "./src/api/utils/utils.js";
 
 const app = express(); // Creacion de app en express.js
 const PORT = environments.port; // Se usa el port establecido a la izquierda de la condicion, si se encuentra ocupado, usa el de la derecha
+
+// Seteamos para que el motor de plantillas que utilizara express sera EJS
+app.set("view engine" , "ejs")
+
+// Definimos la ruta donde se encontraran las plantillas ejs brindadas por el backend
+app.set("views", join(__dirname, "src/views"))
+
+// Definimos la ruta que express usara para proveer los archivos staticos de forma publica
+app.use(express.static(join(__dirname, "src/public")))
 
 /////////////////
 // Middlewares //
