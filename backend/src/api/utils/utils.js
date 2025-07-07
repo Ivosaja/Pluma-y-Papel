fe6a4b333e237b89412cc1b0baed28abd4c9252b
LucasSchurkens <schurkenslucas@gmail.com>
2025-07-07 00:00:23 -0300

    Back-end: agregando la carpte utils junto con el utils.js

---
+++ backend/src/api/utils/utils.js
@@ -0,0 +1,15 @@
+import { fileURLToPath } from "url";    //convierte la URL de un archivo a ruta local del sistema de archivos de un SO
+
+import { dirname, join } from "path"; //dirname: para extraer el directorio padre de un archivo
+                                    //Join: une rutas para crear nuevas
+
+const __filename = fileURLToPath(import.meta.url); //contiene la url de un archivo
+
+const __dirname = join(dirname(__filename), "../../../");
+
+
+export{
+
+    __dirname,
+    join
+}
