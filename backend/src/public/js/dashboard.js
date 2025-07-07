6e2e61da88adfca1b6ce54d308fd06337183c4e6
Ivosaja <ivoliobel@gmail.com>
2025-07-07 02:02:09 -0300

    Backend: Finalizando vista dashboard.ejs, junto con el agregado de la carpeta public con sus modulos css,js e img para servir archivos estaticos desde el servidor para dashboard.ejs, y eliminacion de fetch en dashboard.js

--- backend/src/public/js/dashboard.js
+++ backend/src/public/js/dashboard.js
@@ -1,60 +1,4 @@
-let listaProductos = document.querySelector('.listaProductos')
 const modal = document.getElementById('modal')
-
-async function obtenerTodosLosProductos(){
-    try{
-        let resultado = await fetch('http://localhost:1001/api/products/getAllProducts')
-        let data = await resultado.json();
-        return data
-
-    } catch(err){
-        console.error("Error al obtener todos los productos", err)
-        return []
-    }
-}
-
-function mostrarProductos(arrayProductos){
-    let productos = '';
-    //console.log(arrayProductos)
-
-    if(arrayProductos.length === 0){
-        productos = `<p>No se encontraron productos</p>`
-        alert("No se pudo hacer la peticion correctamente")
-
-    } else{
-        console.log(arrayProductos.message)
-        arrayProductos.payload.forEach(producto => {
-            productos += `
-            <li class="itemProducto">
-                <div class="itemProductoInfo">
-                    <p><strong>ID: </strong>${producto.id_producto}</p>
-                    <img src="${producto.url_imagen}" alt="">
-                    <p><strong>Nombre: </strong>${producto.nombre}</p>
-                    <p><strong>Precio: </strong>$${producto.precio}</p>
-                    <p><strong>Categoria: </strong>${producto.categoria}</p>
-                    <p>
-                        <strong>Activo: </strong>
-                        <span class="${producto.activo === 1 ? 'activo' : 'inactivo'}">${producto.activo === 1 ? true : false}</span>
-                    </p>
-                </div>
-                <div class="acciones">
-                    <button type="button" onclick="modificarProducto(${producto.id_producto})" id="btn-modificarProducto">
-                        <i class="fas fa-pen iconModificar"></i>
-                    </button>
-                    <button type="button" onclick="eliminarProducto(${producto.id_producto})">
-                        <i class="fas fa-trash iconEliminar"></i>
-                    </button>
-                    <button type="button" onclick="activarProducto(${producto.id_producto})">
-                        <i class="fas fa-check iconActivar"></i>
-                    </button>
-                </div>
-            </li>
-            `
-        })
-    }
-
-    listaProductos.innerHTML = productos;
-}
 
 function modificarProducto(){
     window.location.href = '../modificarProducto/modificarProducto.html'
@@ -163,8 +107,7 @@ function usarModal(tipo, idProducto){
 
             setTimeout(async () => {
                 ocultarModal();
-                const productos = await obtenerTodosLosProductos();
-                mostrarProductos(productos);
+                window.location.reload() // Reload recargar la pagina para que el HTML renderizado por el servidor vuelva a cargar con los cambios hechos
             }, 3000);
         })
 
@@ -203,8 +146,7 @@ function usarModal(tipo, idProducto){
 
             setTimeout(async() => {
                 ocultarModal()
-                const productos = await obtenerTodosLosProductos()
-                mostrarProductos(productos)
+                window.location.reload() // Reload recargar la pagina para que el HTML renderizado por el servidor vuelva a cargar con los cambios hechos
             }, 3000)
         })
 
@@ -230,11 +172,6 @@ function ocultarModal(){
 
 
 async function init(){
-    listaProductos.innerHTML = `<p>Cargando productos...</p>`
-
-    let productos = await obtenerTodosLosProductos()
-    mostrarProductos(productos)
-
     agregarProducto()
 
     volverProductos();
