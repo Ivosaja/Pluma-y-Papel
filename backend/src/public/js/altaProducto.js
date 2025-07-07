3c5b62ddc999ea2cd82cae6e7443fc0013553eec
Ivosaja <ivoliobel@gmail.com>
2025-07-07 02:44:34 -0300

    Backend: agregando estilos y archivos.js a carpeta publica para que el backend sirva esos archivos estaticos

---
+++ backend/src/public/js/altaProducto.js
@@ -0,0 +1,132 @@
+let btnVolver = document.getElementById("volverDashboard");
+
+let altaProductoForm = document.getElementById("altaProductoForm");
+
+let contenedorModalAlta =document.getElementById("contenedorModalAltaProducto")
+
+
+function volverDashboard(){
+
+    btnVolver.addEventListener("click", function(){
+
+        window.location.href = "../dashboard/dashboard.html";
+    })
+}
+
+
+
+function enviarFormualrio(){
+    altaProductoForm.addEventListener("submit", async(event)=>{
+        event.preventDefault();
+
+        let formData = new FormData(event.target);
+
+        let data = Object.fromEntries(formData.entries());
+
+        if(!data.nombre || !data.url_imagen || !data.precio || !data.categoria){
+
+            usarModal("incorrecto", "Todos los campos deben estar completos")
+            mostrarModal();
+            setTimeout(esconderModal, 3000);
+        }
+        else if(parseInt(data.precio)<0 ||  parseInt(data.precio)>500000){ //al llegar desde el form, este llega como string, no como Number
+
+            usarModal("incorrecto", "Los precios no pueden ser negativos ni pueden superar los $10.000.000")
+            mostrarModal();
+
+            setTimeout(esconderModal, 3000);
+        }
+        else{
+            let resultado = await subirProducto(data);
+
+            if(resultado.estado){
+
+                usarModal("correcto", resultado.mensaje)
+                mostrarModal();
+
+                setTimeout(()=>{
+
+                    window.location.href = "../dashboard/dashboard.html";
+                }, 3000)
+            }
+            else{
+
+                usarModal("incorrecto", resultado.mensaje)
+                mostrarModal();
+
+                setTimeout(esconderModal, 2500);
+            }
+            
+        }
+    })
+
+
+
+}
+
+
+async function subirProducto(data){
+    try{
+        let response = await fetch("http://localhost:1001/api/products/postProduct", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json" 
+            },
+            body: JSON.stringify(data)
+        })
+
+
+        if(response.ok){
+            let result = await response.json()
+            return {estado: true, mensaje: result.message};
+        } else{
+            let result = await response.json()
+            return {estado: false, mensaje: result.message};
+        }
+
+    } catch(err){
+        console.error(err);
+        return {estado: false, mensaje: "Error al hacer la peticion"};
+    }
+}
+
+
+function usarModal(estado,mensaje1){
+    let contenido= "";
+
+    if(estado === "correcto"){
+        contenido =
+        `
+        <i class="bi bi-check-circle-fill" id="tilde"></i>
+        <p>${mensaje1}</p>
+        `
+
+
+    }else if(estado==="incorrecto"){
+        contenido =
+        `
+        <i class="fas fa-times" id="cruz"></i>
+        <p>${mensaje1}</p>
+        `
+
+    }
+    contenedorModalAlta.innerHTML = contenido;
+
+}
+
+function mostrarModal(){
+    contenedorModalAlta.style.display = "flex";
+    document.getElementById('contenedor-Principal').classList.add('bloquear-interacciones');
+}
+function esconderModal(){
+    contenedorModalAlta.style.display = "none";
+    document.getElementById('contenedor-Principal').classList.remove('bloquear-interacciones');
+}
+
+function init(){
+    volverDashboard();
+    enviarFormualrio();
+
+}
+
+init();
---
+++ backend/src/public/js/modificarProducto.js
@@ -0,0 +1,143 @@
+const idInput = document.getElementById('idProducto')
+const nombreProducto = document.getElementById('nombreProducto')
+const imagenProducto = document.getElementById('imagenProducto')
+const precioProducto = document.getElementById('precioProducto')
+const categoriaProducto = document.getElementById('categoriaProducto')
+
+const modal = document.getElementById("modal")
+const modalIcono = document.querySelector('.modal-content-icon')
+const modalTexto = document.querySelector('.modal-content-text')
+
+
+async function obtenerProductoClickeadoPorId(id){
+    try{
+        let result = await fetch(`http://localhost:1001/api/products/getProductById/${id}`)
+        let data = await result.json()
+        return data
+
+    } catch(err){
+        console.error(err)
+    }
+}
+
+async function rellenarFormulario(id){
+    try{
+        const result = await obtenerProductoClickeadoPorId(id)
+        const [producto] = result.payload
+        
+        // Mejorar validacion si payload da undefined o 0
+
+        idInput.value = producto.id_producto
+        nombreProducto.value = producto.nombre
+        imagenProducto.value = producto.url_imagen
+        precioProducto.value = producto.precio
+        categoriaProducto.value = producto.categoria
+
+    } catch(err){
+        console.error("Error al rellenar el formulario", err)
+        alert("No se pudo cargar la informacion del producto")
+    }
+}
+
+async function modificarProducto(id, data){
+    try{
+        let result = await fetch(`http://localhost:1001/api/products/modifyProduct/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        let info = await result.json()
+
+        if(result.ok){
+            return {estado: true, mensaje: info.message}
+        } else {
+            return {estado: false, mensaje: info.message}
+        }
+
+    } catch (err){
+        console.error(err)
+        return {estado: false, mensaje: "Error al hacer la peticion"}
+    }
+}
+
+function subirCambios(idProducto){
+    const formModificarProducto = document.getElementById('form-modificarProducto')
+    formModificarProducto.addEventListener('submit', async (event) => {
+        event.preventDefault() // prevengo el envio automatico del formulario
+
+        const formData = new FormData(event.target) // Transformo en una clase que me permite obtener todos los datos de un formulario pasandole por parametro el elemento que disparo el evento (el Formulario)
+
+        const data = Object.fromEntries(formData.entries())
+
+        if(!data.nombre || !data.url_imagen || !data.precio || parseInt(data.precio) < 0 || parseInt(data.precio) > 500000||!data.categoria){
+            mostrarModal("noModificado", "Error, todos los campos deben ser validos y el precio no puede ser negativo ni mayor a $500.000")
+            
+            setTimeout(() => {
+                modal.style.display = 'none'
+                document.getElementById('contenedor-principal').classList.remove('bloquear-interacciones');
+            }, 3000)
+            return
+        }
+
+        let respuestaProductoModificado = await modificarProducto(idProducto, data)
+
+        if(respuestaProductoModificado.estado){
+            mostrarModal("modificado", respuestaProductoModificado.mensaje)
+            
+            
+            setTimeout(() => {
+                modal.style.display = 'none'
+                window.location.href = '../dashboard/dashboard.html'
+                document.getElementById('contenedor-principal').classList.remove('bloquear-interacciones');
+            }, 3000)
+
+        } else{
+            mostrarModal("noModificado", respuestaProductoModificado.mensaje)
+
+            
+            setTimeout(() => {
+                modal.style.display = 'none'
+                document.getElementById('contenedor-principal').classList.remove('bloquear-interacciones');
+            }, 3000)
+        }
+
+    })
+}
+
+function volverDashboardBoton(){
+    let bntVovlerDashboard = document.getElementById("volverDashboard");
+
+    bntVovlerDashboard.addEventListener("click", function(){
+        window.location.href  = "../dashboard/dashboard.html";
+    })
+    
+}
+
+function mostrarModal(tipoModal, mensaje){
+    if(tipoModal === 'modificado'){
+        modalIcono.innerHTML = `<i class="bi bi-check-circle-fill" id="tilde"></i>`
+    } else if(tipoModal === 'noModificado'){
+        modalIcono.innerHTML = '<i class="fas fa-times" id="cruz"></i>'
+    }
+    document.getElementById('contenedor-principal').classList.add('bloquear-interacciones');
+    modal.style.display = 'flex'
+    modalTexto.textContent = mensaje
+}
+
+
+async function init(){
+    const idProducto = sessionStorage.getItem("idProducto")
+    if(idProducto){
+        await rellenarFormulario(idProducto)
+    } else{
+        alert("No se selecciono ningun producto para modificar")
+    }
+
+    subirCambios(idProducto)
+
+    volverDashboardBoton();
+}
+
+init()
